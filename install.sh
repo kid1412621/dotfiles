5d9df2c768d5f2bd3d77f2efd257cb641566f76a
Alex <kid1412621@gmail.com>
2024-08-23 19:21:40 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,54 +1,30 @@
 #!/bin/bash
 
-# Function to detect package manager
-detect_package_manager() {
-  if command -v brew >/dev/null 2>&1; then
-    PKG_MANAGER="brew"
-  elif command -v dnf >/dev/null 2>&1; then
-    PKG_MANAGER="dnf"
-  elif command -v apt >/dev/null 2>&1; then
-    PKG_MANAGER="apt"
-  else
-    echo "No supported package manager found (brew, apt, dnf)."
-    exit 1
-  fi
-}
-
-# Functions for package operations
-install_package() {
-  local package=$1
-  case $PKG_MANAGER in
-    brew)
-      brew install --quiet "$package"
-      ;;
-    dnf)
-      sudo dnf install -y --quiet "$package"
-      ;;
-    apt)
-      sudo apt update -qq && sudo apt install -qq -y "$package"
-      ;;
-  esac
-}
-
-detect_package_manager
-echo "Detected package manager: $PKG_MANAGER"
-
-apps_to_install=(
-  "git"
+APPS_TO_INSTALL=(
   "vim"
   "neovim"
-  "tmux"
   "stow"
+  "tmux"
 )
 
-install_package "${apps_to_install[@]}"
+PKG_MANAGER=$(command -v brew >/dev/null 2>&1 && echo "brew" || command -v dnf >/dev/null 2>&1 && echo "dnf" || command -v apt >/dev/null 2>&1 && echo "apt")
+
+if [ -z "$PKG_MANAGER" ]; then
+  echo "No supported package manager found (brew, apt, dnf)."
+  exit 1
+fi
+
+[ "$PKG_MANAGER" = "brew" ] && brew install --quiet "${APPS_TO_INSTALL[@]}" ||
+[ "$PKG_MANAGER" = "dnf" ] && sudo dnf install -y --quiet "${APPS_TO_INSTALL[@]}" ||
+[ "$PKG_MANAGER" = "apt" ] && sudo apt update -qq && sudo apt install -qq -y "${APPS_TO_INSTALL[@]}"
+
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd "$SCRIPT_DIR"
-echo "$SCRIPT_DIR"
 
 # update vim plugins
 git submodule update --init --recursive
 echo "All Vim plugins updated."
 
+# create symblic links
 stow --adopt .
