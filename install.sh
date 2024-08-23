e4ba86f524183c7d9736d09e70f088ea391e4b76
Alex <kid1412621@gmail.com>
2024-08-23 17:57:35 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@
 detect_package_manager() {
   if command -v brew >/dev/null 2>&1; then
     PKG_MANAGER="brew"
-  elif command -v apt >/dev/null 2>&1; then
-    PKG_MANAGER="apt"
   elif command -v dnf >/dev/null 2>&1; then
     PKG_MANAGER="dnf"
+  elif command -v apt >/dev/null 2>&1; then
+    PKG_MANAGER="apt"
   else
     echo "No supported package manager found (brew, apt, dnf)."
     exit 1
@@ -21,11 +21,11 @@ install_package() {
     brew)
       brew install --quiet "$package"
       ;;
+    dnf)
+      sudo dnf install -y --quiet "$package"
+      ;;
     apt)
       sudo apt update && sudo apt install -y "$package"
-      ;;
-    dnf)
-      sudo dnf install -y "$package"
       ;;
   esac
 }
@@ -34,27 +34,28 @@ detect_package_manager
 echo "Detected package manager: $PKG_MANAGER"
 
 apps_to_install=(
+  "git"
   "vim"
   "neovim"
   "tmux"
+  "stow"
 )
 
 for app in "${apps_to_install[@]}"; do
   install_package "$app"
 done
 
-stow --adopt .
-
 # update vim plugins
-# git submodule update --init --recursive
-PLUGIN_DIR=~/.vim/pack/plugins/start
-for dir in "$PLUGIN_DIR"/*; do
-  if [ -d "$dir/.git" ]; then
-    echo "Updating $(basename "$dir")..."
-    cd "$dir" || exit
-    git pull
-  fi
-done
+git submodule update --init --recursive
+#PLUGIN_DIR=~/.vim/pack/plugins/start
+#for dir in "$PLUGIN_DIR"/*; do
+#  if [ -d "$dir/.git" ]; then
+#    echo "Updating $(basename "$dir")..."
+#    cd "$dir" || exit
+#    git pull
+#  fi
+#done
 
 echo "All Vim plugins updated."
 
+stow --adopt .
