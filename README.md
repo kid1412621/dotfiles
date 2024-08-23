d6faa33b46c15d87a3de6234d31fc334c287ae4f
Alex <kid1412621@gmail.com>
2024-08-23 20:53:38 +0800

    update omz & omb

--- README.md
+++ README.md
@@ -1,17 +1,34 @@
 # dotfiles
 
-## Configs
+## Setups
 
-- vim (plugin via git submodule)
-- neovim (lazyvim)
+### Packages
+
+- git
+- tig
+- lazygit
+- vim
+- neovim
+- bat
+- tmux
+- [oh-my-zsh](https://ohmyz.sh/)/[oh-my-bash](https://ohmybash.nntoan.com/)
+
+### Configs
+
+- .zshrc
+- .bashrc
+- .gitconfig
+- .vim (navtive plugin via git submodule)
+- .config/nvim (lazyvim)
 
 ## Usage
 
 Just Run:
+
 ```bash
 git clone https://github.com/kid1412621/dotfiles.git && dotfiles/install.sh
 ```
 
 Under the hoods, using [stow](https://www.gnu.org/software/stow/) to create symbolic links.
 
-> Note: The intall script only supports package manager: brew, dnf and apt.
+> Note: The intall script only supports package manager: brew, dnf and apt. And make sure the user got install permission.
--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 APPS_TO_INSTALL=(
+  "curl"
+  "git"
+  "tig"
   "vim"
   "neovim"
   "stow"
   "tmux"
+  "bat"
 )
 
 PKG_MANAGER=$(command -v brew >/dev/null 2>&1 && echo "brew" || command -v dnf >/dev/null 2>&1 && echo "dnf" || command -v apt >/dev/null 2>&1 && echo "apt")
@@ -17,6 +21,26 @@ fi
 [ "$PKG_MANAGER" = "brew" ] && brew install --quiet "${APPS_TO_INSTALL[@]}" ||
 [ "$PKG_MANAGER" = "dnf" ] && sudo dnf install -y --quiet "${APPS_TO_INSTALL[@]}" ||
 [ "$PKG_MANAGER" = "apt" ] && sudo apt update -qq && sudo apt install -qq -y "${APPS_TO_INSTALL[@]}"
+
+
+# Determine the current default shell
+case "$SHELL" in
+    */zsh)
+        if [ ! -d "$HOME/.oh-my-zsh" ]; then
+            echo "Installing Oh My Zsh..."
+	    sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
+        fi
+        ;;
+    */bash)
+        if [ ! -d "$HOME/.oh-my-bash" ]; then
+            echo "Installing Oh My Bash..."
+            sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+        fi
+        ;;
+    *)
+        echo "Skip for other shell (not zsh or bash)."
+        ;;
+esac
 
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
