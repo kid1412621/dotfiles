8a5a01bec50d3ec22e8e5b7c87647c49784511bb
Alex <kid1412621@gmail.com>
2024-08-24 20:43:26 +0800

    fix dir

--- script/setup.sh
+++ script/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # for github codespace
+# see: https://docs.github.com/en/codespaces/setting-your-user-preferences/personalizing-github-codespaces-for-your-account#dotfiles
+
+cd "$(dirname "$0")/.."
+
 make
