6979471f2af51ebb61eb1fe75ef12b2661f94f16
alex <kid1412621@gmail.com>
2025-04-01 14:12:42 +0800

    update lazyvim

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+--- snack explorer shows hidden files
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    explorer = {},
+    picker = {
+      sources = {
+        files = { hidden = true },
+        grep = { hidden = true },
+        explorer = { hidden = true },
+      },
+    },
+  },
+};
